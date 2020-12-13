a8b1dee9d41d4384c7887fda8c3f1be2aba531f8
KhaVNguyen <kha.vng@gmail.com>
2020-12-13 03:19:04 -0800

    Revert font size

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,6 +21,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap"
             rel="stylesheet"
           />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1"
+          />
         </Head>
         <body>
           <Main />
